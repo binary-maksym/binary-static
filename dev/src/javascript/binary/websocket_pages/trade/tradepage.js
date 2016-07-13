fe4bd552982a37ba9972735e24c6fff27ff7750e
binary-maksym <maksym@binary.com>
2016-07-13 22:04:37 +0800

    Auto-generated commit

--- dev/src/javascript/binary/websocket_pages/trade/tradepage.js
+++ dev/src/javascript/binary/websocket_pages/trade/tradepage.js
@@ -1,96 +1,98 @@
 var TradePage = (function(){
 
-  var trading_page = 0;
+  // var trading_page = 0;
 
-  var onLoad = function(){
-    if(japanese_client() && /\/trading\.html/i.test(window.location.pathname)) {
-        alert('lol1');
-        window.location.href = page.url.url_for('jptrading');
-        return;
-    } 
-    // else if (!japanese_client() && /jp/.test(window.location.pathname)) {
-    //     alert('lol2');
-    //     window.location.href = page.url.url_for('trading');
-    //     return;
-    // }
-    trading_page = 1;
-    if(sessionStorage.getItem('currencies')){
-      displayCurrencies();
-    }
-    BinarySocket.init({
-      onmessage: function(msg){
-        Message.process(msg);
-      },
-      onclose: function(){
-        processMarketUnderlying();
-      }
-    });
-    Price.clearFormId();
-    TradingEvents.init();
-    Content.populate();
+  // var onLoad = function(){
+  //   if(japanese_client() && /\/trading\.html/i.test(window.location.pathname)) {
+  //       alert('lol1');
+  //       window.location.href = page.url.url_for('jptrading');
+  //       return;
+  //   } 
 
-    if(sessionStorage.getItem('currencies')){
-      displayCurrencies();
-      Symbols.getSymbols(1);
-    }
-    else {
-      BinarySocket.send({ payout_currencies: 1 });
-    }
+  // //   return;
+  // //   // else if (!japanese_client() && /jp/.test(window.location.pathname)) {
+  // //   //     alert('lol2');
+  // //   //     window.location.href = page.url.url_for('trading');
+  // //   //     return;
+  // //   // }
+  // //   trading_page = 1;
+  // //   if(sessionStorage.getItem('currencies')){
+  // //     displayCurrencies();
+  // //   }
+  // //   BinarySocket.init({
+  // //     onmessage: function(msg){
+  // //       Message.process(msg);
+  // //     },
+  // //     onclose: function(){
+  // //       processMarketUnderlying();
+  // //     }
+  // //   });
+  // //   Price.clearFormId();
+  // //   TradingEvents.init();
+  // //   Content.populate();
 
-    if (document.getElementById('websocket_form')) {
-        addEventListenerForm();
-    }
+  // //   if(sessionStorage.getItem('currencies')){
+  // //     displayCurrencies();
+  // //     Symbols.getSymbols(1);
+  // //   }
+  // //   else {
+  // //     BinarySocket.send({ payout_currencies: 1 });
+  // //   }
 
-    // Walktrough Guide
-    Guide.init({
-      script : 'trading'
-    });
-    TradingAnalysis.bindAnalysisTabEvent();
-    $('#tab_portfolio a').text(text.localize('Portfolio'));
-    $('#tab_graph a').text(text.localize('Chart'));
-    $('#tab_explanation a').text(text.localize('Explanation'));
-    $('#tab_last_digit a').text(text.localize('Last Digit Stats'));
-    $('#tab_japan_info a').text(text.localize('Prices'));
-    handleChart();
-  };
+  // //   if (document.getElementById('websocket_form')) {
+  // //       addEventListenerForm();
+  // //   }
 
-  var handleChart = function() {
-    $('#time_duration').text(Content.localize().textDuration + ':');
-    $('#time_period option[value="1t"]').text('1 ' + Content.localize().textTickResultLabel.toLowerCase());
-    $('#time_period option[value="1m"]').text('1 ' + text.localize("minute").toLowerCase());
-    $('#time_period option[value="2m"]').text('2 ' + Content.localize().textDurationMinutes.toLowerCase());
-    $('#time_period option[value="3m"]').text('3 ' + Content.localize().textDurationMinutes.toLowerCase());
-    $('#time_period option[value="5m"]').text('5 ' + Content.localize().textDurationMinutes.toLowerCase());
-    $('#time_period option[value="10m"]').text('10 ' + Content.localize().textDurationMinutes.toLowerCase());
-    $('#time_period option[value="15m"]').text('15 ' + Content.localize().textDurationMinutes.toLowerCase());
-    $('#time_period option[value="30m"]').text('30 ' + Content.localize().textDurationMinutes.toLowerCase());
-    $('#time_period option[value="1h"]').text('1 ' + text.localize('hour').toLowerCase());
-    $('#time_period option[value="2h"]').text('2 ' + Content.localize().textDurationHours.toLowerCase());
-    $('#time_period option[value="4h"]').text('4 ' + Content.localize().textDurationHours.toLowerCase());
-    $('#time_period option[value="8h"]').text('8 ' + Content.localize().textDurationHours.toLowerCase());
-    $('#time_period option[value="1d"]').text('1 ' + text.localize('day').toLowerCase());
+  // //   // Walktrough Guide
+  // //   Guide.init({
+  // //     script : 'trading'
+  // //   });
+  // //   TradingAnalysis.bindAnalysisTabEvent();
+  // //   $('#tab_portfolio a').text(text.localize('Portfolio'));
+  // //   $('#tab_graph a').text(text.localize('Chart'));
+  // //   $('#tab_explanation a').text(text.localize('Explanation'));
+  // //   $('#tab_last_digit a').text(text.localize('Last Digit Stats'));
+  // //   $('#tab_japan_info a').text(text.localize('Prices'));
+  // //   handleChart();
+  // // };
 
-    document.getElementById('time_period').addEventListener("change", function() {
-      setChartSource();
-    });
-  };
+  // // var handleChart = function() {
+  // //   $('#time_duration').text(Content.localize().textDuration + ':');
+  // //   $('#time_period option[value="1t"]').text('1 ' + Content.localize().textTickResultLabel.toLowerCase());
+  // //   $('#time_period option[value="1m"]').text('1 ' + text.localize("minute").toLowerCase());
+  // //   $('#time_period option[value="2m"]').text('2 ' + Content.localize().textDurationMinutes.toLowerCase());
+  // //   $('#time_period option[value="3m"]').text('3 ' + Content.localize().textDurationMinutes.toLowerCase());
+  // //   $('#time_period option[value="5m"]').text('5 ' + Content.localize().textDurationMinutes.toLowerCase());
+  // //   $('#time_period option[value="10m"]').text('10 ' + Content.localize().textDurationMinutes.toLowerCase());
+  // //   $('#time_period option[value="15m"]').text('15 ' + Content.localize().textDurationMinutes.toLowerCase());
+  // //   $('#time_period option[value="30m"]').text('30 ' + Content.localize().textDurationMinutes.toLowerCase());
+  // //   $('#time_period option[value="1h"]').text('1 ' + text.localize('hour').toLowerCase());
+  // //   $('#time_period option[value="2h"]').text('2 ' + Content.localize().textDurationHours.toLowerCase());
+  // //   $('#time_period option[value="4h"]').text('4 ' + Content.localize().textDurationHours.toLowerCase());
+  // //   $('#time_period option[value="8h"]').text('8 ' + Content.localize().textDurationHours.toLowerCase());
+  // //   $('#time_period option[value="1d"]').text('1 ' + text.localize('day').toLowerCase());
 
-  var reload = function() {
-    sessionStorage.removeItem('underlying');
-    window.location.reload();
-  };
+  // //   document.getElementById('time_period').addEventListener("change", function() {
+  // //     setChartSource();
+  // //   });
+  // // };
 
-  var onUnload = function(){
-    trading_page = 0;
-    forgetTradingStreams();
-    BinarySocket.clear();
-    Defaults.clear();
-  };
+  // // var reload = function() {
+  // //   sessionStorage.removeItem('underlying');
+  // //   window.location.reload();
+  // // };
 
-  return {
-    onLoad: onLoad,
-    reload: reload,
-    onUnload : onUnload,
-    is_trading_page: function(){return trading_page;}
-  };
+  // // var onUnload = function(){
+  // //   trading_page = 0;
+  // //   forgetTradingStreams();
+  // //   BinarySocket.clear();
+  // //   Defaults.clear();
+  // // };
+
+  // // return {
+  // //   onLoad: onLoad,
+  // //   reload: reload,
+  // //   onUnload : onUnload,
+  // //   is_trading_page: function(){return trading_page;}
+  // // };
 })();
