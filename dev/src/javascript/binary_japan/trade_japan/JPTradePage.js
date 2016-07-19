ca8beba481b0cbcfde52ba3ea71b5f8cb9f28fb8
binary-maksym <maksym@binary.com>
2016-07-19 18:07:46 +0800

    Auto-generated commit

--- dev/src/javascript/binary_japan/trade_japan/JPTradePage.js
+++ dev/src/javascript/binary_japan/trade_japan/JPTradePage.js
@@ -2,24 +2,25 @@ var JPTradePage = (function() {
 
   var scriptUrl = 'https://binary-com.github.io/japanui/dist/bundle.js';
   var isJapan = true;
-  var documentReady = false;
   var scriptReady = false;
+
+  var getScript = function(cb) {
+    var options = { dataType: 'script', cache: true, url: scriptUrl };
+
+    if (!scriptReady) {
+      $.ajax(options).done(function() {
+        scriptReady = true;
+        cb();
+      });
+    } else {
+      cb();
+    }
+  };
 
   var onLoad = function() {
     isJapan = true;
 
-    // $(function() {
-    //   getScript(function() {
-    //     JapanTrading.start();
-    //     documentReady = true;
-    //   });
-    // });
-
-    // if (documentReady) {
-      getScript(function() {
-        JapanTrading.start();
-      });
-    // }
+    getScript(function() { JapanTrading.start(); });
 
     Content.populate();
     TradingAnalysis.bindAnalysisTabEvent();
@@ -39,23 +40,6 @@ var JPTradePage = (function() {
     isJapan = false;
     JapanTrading.stop();
   };
-
-  function getScript(cb) {
-    var options = {
-      dataType: 'script',
-      cache: true,
-      url: scriptUrl
-    };
-
-    if(!scriptReady){
-      $.ajax(options).done(function(){
-        scriptReady = true;
-        cb();
-      });
-    } else {
-      cb();
-    }
-  }
 
   return {
     onLoad: onLoad,
--- js/binary.js
+++ js/binary.js
@@ -87664,24 +87664,25 @@ pjax_config_page_require_auth("user/portfoliows", function() {
 
   var scriptUrl = 'https://binary-com.github.io/japanui/dist/bundle.js';
   var isJapan = true;
-  var documentReady = false;
   var scriptReady = false;
+
+  var getScript = function(cb) {
+    var options = { dataType: 'script', cache: true, url: scriptUrl };
+
+    if (!scriptReady) {
+      $.ajax(options).done(function() {
+        scriptReady = true;
+        cb();
+      });
+    } else {
+      cb();
+    }
+  };
 
   var onLoad = function() {
     isJapan = true;
 
-    // $(function() {
-    //   getScript(function() {
-    //     JapanTrading.start();
-    //     documentReady = true;
-    //   });
-    // });
-
-    // if (documentReady) {
-      getScript(function() {
-        JapanTrading.start();
-      });
-    // }
+    getScript(function() { JapanTrading.start(); });
 
     Content.populate();
     TradingAnalysis.bindAnalysisTabEvent();
@@ -87701,23 +87702,6 @@ pjax_config_page_require_auth("user/portfoliows", function() {
     isJapan = false;
     JapanTrading.stop();
   };
-
-  function getScript(cb) {
-    var options = {
-      dataType: 'script',
-      cache: true,
-      url: scriptUrl
-    };
-
-    if(!scriptReady){
-      $.ajax(options).done(function(){
-        scriptReady = true;
-        cb();
-      });
-    } else {
-      cb();
-    }
-  }
 
   return {
     onLoad: onLoad,
