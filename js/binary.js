a5901f633ebcab153fddfec21e55b25488f1de02
binary-maksym <maksym@binary.com>
2016-07-20 11:24:53 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -68437,15 +68437,13 @@ texts_json['ZH_TW'] = {"Net_profit":"淨收益","Sorry,_this_feature_is_not_avai
 
 function getAppId() {
   return localStorage.getItem('config.app_id') ? localStorage.getItem('config.app_id') :
-               // /staging\.binary\.com/i.test(window.location.hostname) ? '1098' : '1003';
-               /staging\.binary\.com/i.test(window.location.hostname) ? '1098' : '1003';
+               /staging\.binary\.com/i.test(window.location.hostname) ? '1098' : '1';
 }
 
 function getSocketURL() {
     var server_url = localStorage.getItem('config.server_url');
     if(!server_url) server_url = (/staging\.binary\.com/i.test(window.location.hostname) ? 'www2' : 'ws') + '.binaryws.com';
-    // return 'wss://' + server_url + '/websockets/v3';
-    return 'wss://www.binaryqa15.com/websockets/v3';
+    return 'wss://' + server_url + '/websockets/v3';
 }
 ;var LoggedInHandler = (function() {
     "use strict";
